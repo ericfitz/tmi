acf8daa405219e431a4eb391abc926262a18dfe2
Eric Fitzgerald <github@efitz.net>
2025-09-06 00:38:08 -0400

    Added endpoint enumeration script to test json tool

--- scripts/list-endpoints.sh
+++ scripts/list-endpoints.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-jq -r '.paths | to_entries[] | "\n## \(.key)\n" + (.value | to_entries[] | "- \(.key | ascii_upcase): " + (.value.responses | to_entries | map("\(.key)") | join(", ")))' shared/api-specs/tmi-openapi.json > endpoints.md
+jq -r '.paths | to_entries[] | "\n## \(.key)\n" + (.value | to_entries[] | "- \(.key | ascii_upcase): " + (.value.responses | to_entries | map("\(.key)") | join(", ")))' shared/api-specs/tmi-openapi.json
