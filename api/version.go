a03700ee2789554aa67862e9c5d64d3e6eec79e7
Eric Fitzgerald <github@efitz.net>
2025-10-07 00:09:00 -0400

    chore: Build server binary with version update after feature branch merge

--- api/version.go
+++ api/version.go
@@ -27,7 +27,7 @@ var (
 	// Major version number
 	VersionMajor = "0"
 	// Minor version number
-	VersionMinor = "40"
+	VersionMinor = "41"
 	// Patch version number
 	VersionPatch = "0"
 	// GitCommit is the git commit hash from build
