909100c43409ce873ccbece24699ccd68f1d3b72
Eric Fitzgerald <github@efitz.net>
2025-10-13 16:45:23 -0400

    chore: version bump due to build

--- api/version.go
+++ api/version.go
@@ -27,7 +27,7 @@ var (
 	// Major version number
 	VersionMajor = "0"
 	// Minor version number
-	VersionMinor = "59"
+	VersionMinor = "60"
 	// Patch version number
 	VersionPatch = "0"
 	// GitCommit is the git commit hash from build
