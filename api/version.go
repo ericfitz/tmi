3e9cfac4399235e7e0cd5543c903aa7d24d8653a
Eric Fitzgerald <github@efitz.net>
2025-10-26 15:55:22 -0400

    chore(deps): bump

--- api/version.go
+++ api/version.go
@@ -27,7 +27,7 @@ var (
 	// Major version number
 	VersionMajor = "0"
 	// Minor version number
-	VersionMinor = "75"
+	VersionMinor = "76"
 	// Patch version number
 	VersionPatch = "0"
 	// GitCommit is the git commit hash from build
