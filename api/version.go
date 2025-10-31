d4eee3c3d1c936637cda3e1206b5373afdd61b5e
Eric Fitzgerald <github@efitz.net>
2025-10-31 17:35:43 -0400

    chore: formatting

--- api/version.go
+++ api/version.go
@@ -27,7 +27,7 @@ var (
 	// Major version number
 	VersionMajor = "0"
 	// Minor version number
-	VersionMinor = "121"
+	VersionMinor = "122"
 	// Patch version number
 	VersionPatch = "0"
 	// GitCommit is the git commit hash from build
