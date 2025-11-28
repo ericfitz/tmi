579e4df149cbe956d1a23a5e1e1de6677bacac37
Eric Fitzgerald <github@efitz.net>
2025-11-28 11:34:22 -0500

    chore: documentation cleanup

--- docs/archive/migrations/006_addons.down.sql
+++
@@ -1,9 +0,0 @@
--- Drop addon_invocation_quotas table
-DROP TRIGGER IF EXISTS update_addon_invocation_quotas_modified_at ON addon_invocation_quotas;
-DROP TABLE IF EXISTS addon_invocation_quotas CASCADE;
-
--- Drop addons table
-DROP TABLE IF EXISTS addons CASCADE;
-
--- Drop administrators table
-DROP TABLE IF EXISTS administrators CASCADE;
--- docs/archive/migrations/008_user_api_quotas.down.sql
+++
@@ -1,4 +0,0 @@
--- Drop user_api_quotas table
-DROP TRIGGER IF EXISTS update_user_api_quotas_modified_at ON user_api_quotas;
-DROP INDEX IF EXISTS idx_user_api_quotas_user_id;
-DROP TABLE IF EXISTS user_api_quotas;
