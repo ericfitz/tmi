b85386083c8de2ee1d54d068515af336df097dc1
Eric Fitzgerald <github@efitz.net>
2026-01-17 19:51:53 -0500

    chore: delete backup file

--- api/version.go
+++ api/version.go
@@ -29,7 +29,7 @@ var (
 	// Minor version number
 	VersionMinor = "257"
 	// Patch version number
-	VersionPatch = "3"
+	VersionPatch = "4"
 	// GitCommit is the git commit hash from build
 	GitCommit = "development"
 	// BuildDate is the build timestamp
